0e0ef04369e4a01f5ec9dc5b45f2fa26c5f9feb5
sedziafa <v2sedziafa@gmail.com>
2023-12-15 21:05:08 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main - this is the main action
  *
- * return: always 0
+ * Return: always 0
  */
 
 int main(void)
-{       
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+	return (0);
 }
